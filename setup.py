f17836d8b613807136c340b4e701fb0484fc3759
keyvan <ksalehi56@yahoo.com>
2025-11-26 15:08:10 +1300

    push first demo

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='mlops projects on azure',
+    version='0.0.1',
+    author='keyvan',
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ src/logger.py
@@ -0,0 +1,13 @@
+import logging
+import os
+from pathlib import Path
+from datetime import datetime
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_dir=Path(__file__).resolve().parent.parent / 'logs' 
+logs_dir.mkdir(exist_ok=True)
+log_path_file=logs_dir / LOG_FILE
+logging.basicConfig(
+    filename=log_path_file,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s"  ,
+    level=logging.INFO
+)
